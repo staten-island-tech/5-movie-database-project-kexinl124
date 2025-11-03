133721e8a613ce80940826d0be62064143d3e9ca
kexinl124 <KExinl10@nycstudents.net>
2025-11-03 09:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+for index,item in enumerate(data):
+    print(index, ":", (item)["title"])
+    
+
+choice = int(input("Give a year:"))
+for i in enumerate (data):
+    if (item)["year"] > choice:
+         print((item)["title"])
